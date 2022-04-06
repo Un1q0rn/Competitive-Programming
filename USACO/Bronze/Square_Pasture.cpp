e26741784284d47a9c9c5eea069fc3c9938a8dc8
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-04-06 16:41:17 +0700

    Update Square_Pasture.cpp

--- USACO/Bronze/Square_Pasture.cpp
+++ USACO/Bronze/Square_Pasture.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi(x)  vector<x>;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+#define ll long long int
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
+    if (sz(name)) {
+        freopen((name + ".in").c_str(), "r", stdin); // see /general/input-output
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("square");
+    cin.tie(0)->sync_with_stdio(0);
+    int x1,y1,x2,y2,x3,y3,x4,y4;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    int len = max(x2,x4)-min(x1,x3);
+    int wid = max(y2,y4)-min(y1,y3);
+    cout << max(len,wid)*max(len,wid);
+    return 0;
+}
 
