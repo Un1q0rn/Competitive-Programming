f63246206076072e7e6dcbed93d4790f45f1d294
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-04-10 10:48:57 +0700

    Create 0042.cpp

---
+++ Programming.in.th/0042.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int q;cin >> q;
+    while(q--){
+        long double n;
+        cin >> n;
+        cout << fixed << setprecision(0) << pow(3,n) << "\n";
+    }
+    return 0;
+}
