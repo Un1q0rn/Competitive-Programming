afd1f6ad10d8e451af2cd0890f9ebb9b0636161b
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-04-11 02:01:06 +0700

    Create tumso17_linear.cpp

---
+++ Programming.in.th/tumso17_linear.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    ll a,b;cin >> a >> b;
+    cout << b/a;
+    return 0;
+}
