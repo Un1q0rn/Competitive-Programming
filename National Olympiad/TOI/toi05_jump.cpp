4e94504e9299a63507e61c6adf1d8dc89eb60bce
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-05-04 15:54:23 +0700

    Create toi05_jump.cpp

---
+++ National Olympiad/TOI/toi05_jump.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define pi pair<int,int>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define f first
+#define s second 
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    int n,k;cin >> n >> k;
+    vector<int> a(n);
+    for(auto &e:a) cin >> e;
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        int a1 = upper_bound(a.begin(),a.end(),a[i])-a.begin();
+        int a2 = upper_bound(a.begin(),a.end(),a[i]+k)-a.begin();
+        ans = max(ans,a2-a1);
+    }
+    cout << ans;
+    return 0;
+}
