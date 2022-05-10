ec109f05c954e1d95622436382fe082beb47e998
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-05-10 14:42:54 +0700

    Create Daisy Chains.cpp

---
+++ USACO/Bronze/Daisy Chains.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi(x)  vector<x>
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+#define ll long long int
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
+    if (sz(name)) {
+        freopen((name + ".in").c_str(), "r", stdin); // see /general/input-output
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("");
+    int n;cin >> n;
+    vector<int> a(n);
+    for(auto  &e:a) cin >> e;
+    int ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int sum=0;
+            for(int k=i;k<=j;k++){
+                sum+=a[k];
+            }
+            int found = 0;
+            for(int k=i;k<=j;k++){
+                if(a[k]*(j-i+1) == sum) found = 1;
+            }
+            if(found) ans++;
+        }
+    }
+    cout << ans;
+    return 0;
+}
