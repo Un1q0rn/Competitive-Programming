2cb0bdf91b30331d8dcbc57e25d432ee51b672ae
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-05-11 09:57:04 +0700

    Create Hoof, Paper, Scissors.cpp

---
+++ USACO/Silver/Hoof, Paper, Scissors.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi(x)  vector<x>
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+#define ll long long int
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
+    if (sz(name)) {
+        freopen((name + ".in").c_str(), "r", stdin); // see /general/input-output
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("hps");
+    int n;cin >> n;
+    vector<int> h(n+2),s(n+2),p(n+2);
+    for(int i=1;i<=n;i++){
+        char c;cin >> c;
+        if(c == 'P'){
+            s[i]++;
+        }
+        if(c == 'H'){
+            p[i]++;
+        }
+        if(c == 'S'){
+            h[i]++;
+        }
+        h[i]+=h[i-1],p[i]+=p[i-1],s[i]+=s[i-1];
+    }
+    int ans = 0;
+    for(int i=1;i<=n;i++){
+       int sum1 = max({p[i],s[i],h[i]});
+       int sum2 = max({p[n]-p[i],s[n]-s[i],h[n]-h[i]});
+       ans = max(sum1+sum2,ans);
+    }
+    cout << ans << '\n';
+    return 0;
+}
