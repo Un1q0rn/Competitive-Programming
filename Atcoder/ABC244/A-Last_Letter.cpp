b0c7b11298a0d0215ec1c8bfc882730d3d057e61
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-05-23 17:56:36 +0700

    Create A-Last_Letter.cpp

---
+++ Atcoder/ABC244/A-Last_Letter.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define pi pair<int,int>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    int n;cin >> n;
+    string s;cin >> s;
+    cout << s[sz(s)-1];
+    return 0;
+}
