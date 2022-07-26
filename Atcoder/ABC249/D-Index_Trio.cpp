deb17dabd45fded6a26a64a62a6cc1f382cc9c99
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-07-26 08:16:57 +0700

    Create D-Index_Trio.cpp

---
+++ Atcoder/ABC249/D-Index_Trio.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ull unsigned long long int
+#define ll long long int 
+#define ld long double
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,b,a) for(int i=b;i>=a;i--)
+#define vi(x)  vector<x>
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define setdecimal(x) cout << fixed << setprecision(x)
+ 
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+ll gcd(ll a,ll b){
+    if(!(a%b)) return b;
+    return gcd(b,a%b);
+}
+ll lcm(ll a,ll b){
+    return a*b/gcd(a,b);
+}
+const int N = 2e5+10;
+void solve(){
+    int n;cin >> n;
+    vector<int> cnt(N);
+    FOR(i,1,n){
+        int x;cin >> x;
+        cnt[x]++;
+    }
+    ll ans = 0;
+    FOR(i,1,N-10){
+        for(int j=1;i*j<=N-10;j++){
+            ans+=(ll)cnt[i]*cnt[j]*cnt[i*j];
+        }
+    }
+    cout << ans;
+} 
+
+bool multi = false;
+
+int main()
+{
+    cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+    int t = 1;
+    if(multi) cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
