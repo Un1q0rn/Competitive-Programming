c21d77fb3803186407c8b62c7fde38f7a64ee145
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-07-29 17:18:49 +0700

    Create B-Get_Closer.cpp

---
+++ Atcoder/ABC246/B-Get_Closer.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ull unsigned long long int
+#define ll long long int 
+#define ld long double
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,b,a) for(int i=b;i>=a;i--)
+#define vi(x)  vector<x>
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define setdecimal(x) cout << fixed << setprecision(x)
+ 
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+ll gcd(ll a,ll b){
+    if(!(a%b)) return b;
+    return gcd(b,a%b);
+}
+ll lcm(ll a,ll b){
+    return a*b/gcd(a,b);
+}
+void solve(){
+    double x,y;cin >> x >> y;
+    double d = sqrt((x*x+y*y) );
+    cout << x/d << ' ' << y/d ;
+} 
+
+
+bool multi = false;
+
+int main()
+{
+    cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+    setdecimal(12);
+    int t = 1;
+    if(multi) cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
