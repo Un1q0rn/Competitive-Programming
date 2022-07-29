1a5c8afd97165f33b18ffedd55492a9398003549
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-07-29 17:19:10 +0700

    Create C-Coupon.cpp

---
+++ Atcoder/ABC246/C-Coupon.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ull unsigned long long int
+#define ll long long int 
+#define ld long double
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,b,a) for(int i=b;i>=a;i--)
+#define vi(x)  vector<x>
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define setdecimal(x) cout << fixed << setprecision(x)
+ 
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+ll gcd(ll a,ll b){
+    if(!(a%b)) return b;
+    return gcd(b,a%b);
+}
+ll lcm(ll a,ll b){
+    return a*b/gcd(a,b);
+}
+void solve(){
+    int n,k,x;cin >> n >> k >> x;
+    vector<int> a(n);
+    for(auto &e:a) cin >> e;
+    sort(all(a),greater<int>());
+    ll ans = 0;
+    vector<int> b;
+    FOR(i,0,n-1){
+        if(k){
+            int dis = min((int)((floor)((double)a[i]/x)),k);
+            a[i] -= x*dis;
+            k-=dis;
+        }
+        b.pb(a[i]);
+    }
+    sort(all(b),greater<int>());
+    FOR(i,0,n-1){
+        if(k){
+            k--;
+            continue;
+        }
+        else ans+=b[i];
+    }
+    cout << ans;
+} 
+
+
+bool multi = false;
+
+int main()
+{
+    cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+    setdecimal(12);
+    int t = 1;
+    if(multi) cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
