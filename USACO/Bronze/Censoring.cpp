d6a0e28a9d2a712ed20146207041122d7055cc85
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-08-02 10:01:31 +0700

    Create Censoring.cpp

---
+++ USACO/Bronze/Censoring.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi(x)  vector<x>
+#define pb push_back
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,b,a) for(int i=b;i>=a;i--)
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define pb push_back
+#define setdecimal(x) cout << fixed << setprecision(x)
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+#define ll long long int
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
+    if (sz(name)) {
+        freopen((name + ".in").c_str(), "r", stdin); // see /general/input-output
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+int main() {
+    setIO("censor");
+    string s,t;cin >> s >> t;
+    string tmp;
+    for(auto c:s){
+        tmp+=c;
+        if(sz(tmp) > sz(t) && tmp.substr(sz(tmp)-sz(t)) == t ){
+            tmp.resize(sz(tmp)-sz(t));
+        }
+    }
+    cout << tmp;
+    return 0;
+}
