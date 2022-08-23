3ef5de3b1707c821dcdb1f56c141e13504fad367
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-08-23 13:08:01 +0700

    Create Towers.cpp

---
+++ CSES/Sorting and Searching/Towers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;cin >> n;
+  vector<int> a(n);
+  for(auto &e:a) cin >> e;
+  vector<int> lis;
+  for(auto e:a){
+      int pos = upper_bound(lis.begin(),lis.end(),e)-lis.begin();
+      if(pos == lis.size()){
+          lis.push_back(e);
+      }
+      else{
+          lis[pos] = e;
+      }
+  }
+  cout << lis.size();
+  return 0;
+}
