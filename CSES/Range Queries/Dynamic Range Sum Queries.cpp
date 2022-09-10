7807457d4679cbb79e73143672c00cdc26733398
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-09-10 18:34:46 +0700

    Create Dynamic Range Sum Queries.cpp

---
+++ CSES/Range Queries/Dynamic Range Sum Queries.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T>
+struct Segtree{
+    T comp(T a,T b) {return a+b;}
+    const T DEFAULT = 0;
+    vector<T> t;
+    int n;
+    Segtree(int n) : t(2*n,DEFAULT),n(n){}
+    void set(int p,T val){
+        if(p < 0 || p >= n) return ;
+        for(t[p+=n] = val;p > 1;p>>=1){t[p >> 1] = comp(t[p],t[p^1]);}
+    }
+    T qry(int l,int r){
+        if(l < 0 || l >= n || r <= 0 || r < l) return 0;
+        T res = DEFAULT;
+        for(l+=n,r+=n;l < r;l >>= 1,r >>= 1){
+            if(l&1) res = comp(res,t[l++]);
+            if(r&1) res = comp(res,t[--r]);
+        }
+        return res;
+    }
+    
+};
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n,q;cin >> n >> q;
+  Segtree<ll> segtree(n);
+  for(int i=0;i<n;i++){
+      int x;cin >> x;
+      segtree.set(i,x);
+  }
+  while(q--){
+      int opr;cin >> opr;
+      if(opr == 1){
+          int k,u;cin >> k >> u;
+          segtree.set(k-1,u);
+      }
+      else{
+          int l,r;cin >> l >> r;
+          cout << segtree.qry(l-1,r) << '\n';
+      }
+  }
+  return 0;
+}
