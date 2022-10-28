f1a01dfc62ffac4328e19a471f692c8a4a6af388
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-10-28 15:49:52 +0700

    Create C-Submask.cpp

---
+++ Atcoder/ABC269/C-Submask.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ull unsigned long long int
+#define ll long long int 
+#define ld long double
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,b,a) for(int i=b;i>=a;i--)
+#define vi(x)  vector<x>
+#define pb push_back
+#define pf push_front
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define setdecimal(x) cout << fixed << setprecision(x)
+ 
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+ll gcd(ll a,ll b){
+    return b == 0 ? a:gcd(b,a%b);
+}
+void solve(){
+    ll x;cin >> x;
+    vector<ll> res;
+    for(ll i=(1ll<<60)-1;i>=0;i--){
+        i&=x;
+        res.pb(i);
+    }
+    reverse(all(res));
+    for(auto e:res){
+        cout << e << '\n';
+    }
+}
+
+
+bool multi = false;
+
+int main()
+{
+    cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+    int t = 1;
+    if(multi) cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
