2e2ec74df9fd401f72874099b92b5eeb9f69ee9d
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-11-13 15:14:20 +0700

    Create A-^{-1}.cpp

---
+++ Atcoder/ABC277/A-^{-1}.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ull unsigned long long int
+#define ll long long int 
+#define ld long double
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,b,a) for(int i=b;i>=a;i--)
+#define vi(x)  vector<x>
+#define pb push_back
+#define pf push_front
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define setdecimal(x) cout << fixed << setprecision(x)
+ 
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+ll gcd(ll a,ll b){
+    return b == 0 ? a:gcd(b,a%b);
+}
+void solve(){
+    int n,x;cin >> n >> x;
+    vector<int> a(n);
+    for(auto &e:a) cin >> e;
+    for(int i=0;i<n;i++) {
+        if(a[i] == x){
+            cout << i+1;
+            return ;
+        }
+    }
+}
+
+bool multi = false;
+
+int main()
+{
+    cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+    int t = 1;
+    if(multi) cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
