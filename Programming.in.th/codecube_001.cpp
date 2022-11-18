f15c31bf99b60174d3855ffda88dc3397195c820
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2022-11-18 17:01:07 +0700

    Create codecube_001.cpp

---
+++ Programming.in.th/codecube_001.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+#define vi(x)  vector<x>
+#define pb push_back
+#define pf push_front
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,b,a) for(int i=b;i>=a;i--)
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define setdecimal(x) cout << fixed << setprecision(x)
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+#define ll long long 
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    ll a,b;cin >> a >>b;
+    cout << a*b;
+    return 0;
+}
