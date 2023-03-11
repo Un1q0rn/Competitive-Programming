7f14f06a40744cd617a596d21fbd3288450cdce5
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2023-03-12 00:41:57 +0700

    Update SAPO15_jazz.cpp

--- National Olympiad/SAPO/SAPO15_jazz.cpp
+++ National Olympiad/SAPO/SAPO15_jazz.cpp
@@ -25,7 +25,7 @@ int main()
             dp[i][i+j] = dp[i+1][i+j]+1;
             for(int k=i+1;k<=i+j;k++){
                 if(s[i] != s[k]) continue;
-                if(s[i])dp[i][i+j] = min(dp[i][i+j],dp[i+1][k-1]+dp[k+1][i+j]);
+                dp[i][i+j] = min(dp[i][i+j],dp[i+1][k-1]+dp[k+1][i+j]);
             }
         }
     }
