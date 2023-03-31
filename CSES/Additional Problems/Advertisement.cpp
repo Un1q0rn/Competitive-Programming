0f7a133cf19ebd16379ec198b5c92ca1d7731c6d
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2023-03-31 21:44:54 +0700

    Create Advertisement.cpp

---
+++ CSES/Additional Problems/Advertisement.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(x) begin(x),end(x)
+#define pb push_back
+#define sz(x) (int)x.size()
+#define s second
+#define f first
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;cin >> n;
+    vector<ll> a(n);
+    for(auto &e:a) cin >> e;
+    stack<ll> st;
+    vector<ll> ans(n);
+    for(int i=0;i<n;i++){
+        while(sz(st) && a[st.top()] >= a[i]) st.pop();
+        int len = i-(sz(st) ? st.top() : -1);
+        ans[i] += a[i]*len;
+        st.push(i);
+    }
+    while(sz(st)) st.pop();
+    for(int i=n-1;i>=0;i--){
+        while(sz(st) && a[st.top()] >= a[i]) st.pop();
+        int len = (sz(st) ? st.top() : n) - i;
+        ans[i] += a[i]*(len-1);
+        st.push(i);
+    }
+    cout << *max_element(all(ans));
+    return 0;
+}
