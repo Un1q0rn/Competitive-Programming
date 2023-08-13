47ce8204dd50b04538efa5217c080b445e850ebc
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2023-08-13 19:22:43 +0700

    Create Simple Array Sum.cpp

---
+++ HackerRank/Simple Array Sum.cpp
@@ -0,0 +1,11 @@
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+int simpleArraySum(vector<int> ar) {
+    int res = accumulate(ar.begin(),ar.end(),0);
+    return res;
+}
