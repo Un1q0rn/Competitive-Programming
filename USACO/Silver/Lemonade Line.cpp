bffa3e17e39ee6735f0ed59610b5bdac3dfc4bd0
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2024-06-17 15:04:03 +0700

    Create Lemonade Line.cpp

---
+++ USACO/Silver/Lemonade Line.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+#define vi(x)  vector<x>
+#define pb push_back
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define ROF(i,b,a) for(int i=b;i>=a;i--)
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define pb push_back
+#define setdecimal(x) cout << fixed << setprecision(x)
+
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+#define ll long long int
+
+void setIO(string name = "") {
+    cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
+    if (sz(name)) {
+        freopen((name + ".in").c_str(), "r", stdin); // see /general/input-output
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int main() {
+    setIO("lemonade");
+    int n;cin >> n;
+    vector<int> a(n);
+    for(auto &e:a) cin >> e;
+    sort(all(a),greater<int>());
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        cnt += (cnt <= a[i]);
+    }
+    cout << cnt;
+    return 0;
+}
