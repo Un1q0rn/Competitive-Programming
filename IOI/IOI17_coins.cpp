b08bbc5cd70dcae08436cbcdac94470fd554ebc6
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2024-09-27 01:08:43 +0700

    Update IOI17_coins.cpp

--- IOI/IOI17_coins.cpp
+++ IOI/IOI17_coins.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+ 
+vector<int> coin_flips(vector<int> b,int c){
+    int p = 0;
+    for(int i=0;i<64;i++){
+        if(b[i]) continue;
+        p ^= i;
+    }
+    c ^= p;
+    return {c};
+}
+
+int find_coin(vector<int> b){
+    int p = 0;
+    for(int i=0;i<64;i++){
+        if(b[i]) p ^= i;
+    }
+    return p;
+}
