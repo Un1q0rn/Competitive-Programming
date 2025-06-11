498fc891798781e4b7b23fb7bc68bd81d4d2e396
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2025-06-11 14:58:55 +0700

    Create 0001.c

---
+++ Programming.in.th/0001.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    
+    int sum = a + b + c;
+    
+    if (sum >= 80) printf("A\n");
+    else if (sum >= 75) printf("B+\n");
+    else if (sum >= 70) printf("B\n");
+    else if (sum >= 65) printf("C+\n");
+    else if (sum >= 60) printf("C\n");
+    else if (sum >= 55) printf("D+\n");
+    else if (sum >= 50) printf("D\n");
+    else printf("F\n");
+    
+    return 0;
+}
