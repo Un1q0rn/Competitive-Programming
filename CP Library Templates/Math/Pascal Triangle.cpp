c0f1af1d3cc8b68e02da9ce4a712b2c4b4c8c18d
Nonthapat Sriboonruang <85183458+Un1q0rn@users.noreply.github.com>
2025-07-16 15:36:35 +0700

    Update Pascal Triangle.cpp

--- CP Library Templates/Math/Pascal Triangle.cpp
+++ CP Library Templates/Math/Pascal Triangle.cpp
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binomial(int n, int k) {
-	vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+#define ll long long
 
-	for (int i = 0; i <= n; i++) {
-		dp[i][0] = 1;
-		if (i <= k) { dp[i][i] = 1; }
-	}
+const int MAXN = 2e3;
 
-	for (int i = 0; i <= n; i++) {
-		for (int j = 1; j <= min(i, k); j++) {
-			if (i != j) {
+ll dp[MAXN + 10][MAXN + 10];
+void binomial() {
+	for(int i = 0; i <= MAXN; i++){
+        for(int j = 0; j <= i; j++){
+            if(j == 0 || i == j){
+                dp[i][j] = 1;
+            }
+            else{
 				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]);
 			}
 		}
-	}
-
-	return dp[n][k];
+    }
 }
-
 int main() {
-  cin.tie(0)->sync_with_stdio(0);
-	int n,k;cin >> n >> k;
-  cout << binomial(n,k);
+    cin.tie(0)->sync_with_stdio(0);
+    binomial();
+    int n,k;cin >> n >> k;
+    cout << dp[n][k];
 }
